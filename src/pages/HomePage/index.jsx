9617aaff6f049d61fc9682f251027b1819a8b788
Sára Hölclová <sara.holclova@gmail.com>
2024-05-11 17:19:20 +0200

    react ve velkém

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import { TShirt } from '../components/TShirt';
 import { TShirtSetup } from '../components/TShirtSetup';
 import './style.css';
 
 export const HomePage = () => {
+  const [typ, setTyp] = useState('');
+  const [barva, setBarva] = useState('');
+  const [potisk, setPotisk] = useState('');
+
+  const typeChange = (type) => {
+    setTyp(type);
+  };
+
+  const colorChange = (color) => {
+    setBarva(color);
+  };
+
+  const printChange = (print) => {
+    setPotisk(print);
+  };
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={typ} color={barva} print={potisk} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup
+            onTypeChange={typeChange}
+            onColorChange={colorChange}
+            onPrintChange={printChange}
+          />
         </div>
       </div>
     </div>
--- src/pages/components/TShirtSetup/index.jsx
+++ src/pages/components/TShirtSetup/index.jsx
@@ -1,10 +1,15 @@
 import './style.css';
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({ onTypeChange, onColorChange, onPrintChange }) => {
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
-      <select className="tshirt-setup__input">
+      <select
+        onChange={(event) => {
+          onTypeChange(event.target.value);
+        }}
+        className="tshirt-setup__input"
+      >
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -14,15 +19,25 @@ export const TShirtSetup = () => {
       </select>
 
       <label className="tshirt-setup__label">Barva:</label>
-      <select className="tshirt-setup__input">
+      <select
+        onChange={(event) => onColorChange(event.target.value)}
+        className="tshirt-setup__input"
+      >
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
         <option value="green">Zelená</option>
       </select>
-    
+
       <label className="tshirt-setup__label">Potisk:</label>
-      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" />
+      <input
+        onChange={(event) => {
+          onPrintChange(event.target.value);
+        }}
+        className="tshirt-setup__input"
+        type="text"
+        defaultValue="Ahoj světe"
+      />
     </form>
   );
 };
